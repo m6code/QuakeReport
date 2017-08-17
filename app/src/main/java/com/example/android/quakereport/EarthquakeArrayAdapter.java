6e8df7cbb45a164fd04784ea30e76e03ea036d32
Benjamin M6 <benjamynfrank101@gmail.com>
2017-08-17 03:37:48 -0500

    Add magnitude circle 	add color code for the earthquake magnitude 	Add code to make the magnitude textview display colors based on the magnitude of the earth quake

--- app/src/main/java/com/example/android/quakereport/EarthquakeArrayAdapter.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeArrayAdapter.java
@@ -1,8 +1,10 @@
 package com.example.android.quakereport;
 
 import android.content.Context;
+import android.graphics.drawable.GradientDrawable;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -100,6 +102,16 @@ public class EarthquakeArrayAdapter extends ArrayAdapter<Earthquake> {
         String formattedDate = formatDate(dateObject);
         tv_date.setText(formattedDate);
 
+        // Set the proper background color on the magnitude circle
+        // Fetch the background from the TextView, which is a GradientDrawable
+        GradientDrawable magnitudeCircle = (GradientDrawable)tv_magnitude.getBackground();
+
+        // Get the appropriate background color based on the current earthquake magnitude
+        int magnitudeColor = getMagnitudeColor(currentEarthquake.getMagnitude());
+
+        // Set the color on the magnitude circle
+        magnitudeCircle.setColor(magnitudeColor);
+
         // Return the list item view
         return listItemView;
     }
@@ -135,5 +147,47 @@ public class EarthquakeArrayAdapter extends ArrayAdapter<Earthquake> {
     private String formatMagnitude(double magnitude){
         DecimalFormat magnitudeFormat = new DecimalFormat("0.0");
         return magnitudeFormat.format(magnitude);
+    }
+
+    private int getMagnitudeColor(double magnitude){
+        int magnitudeColorResourceId;
+        int magnitudeFloor = (int)Math.floor(magnitude); // rounds the magnitude from double to integer
+        // check the magnitude of the earthquake and sets the
+        // appropriate color background for the magnitude_circle.xml
+        switch (magnitudeFloor){
+            case 0:
+            case 1:
+                magnitudeColorResourceId = R.color.magnitude1;
+                break;
+            case 2:
+                magnitudeColorResourceId = R.color.magnitude2;
+                break;
+            case 3:
+                magnitudeColorResourceId = R.color.magnitude3;
+                break;
+            case 4:
+                magnitudeColorResourceId = R.color.magnitude4;
+                break;
+            case 5:
+                magnitudeColorResourceId = R.color.magnitude5;
+                break;
+            case 6:
+                magnitudeColorResourceId = R.color.magnitude6;
+                break;
+            case 7:
+                magnitudeColorResourceId = R.color.magnitude7;
+                break;
+            case 8:
+                magnitudeColorResourceId = R.color.magnitude8;
+                break;
+            case 9:
+                magnitudeColorResourceId =R.color.magnitude9;
+                break;
+            default:
+                magnitudeColorResourceId =R.color.magnitude10plus;
+                break;
+        }
+
+        return ContextCompat.getColor(getContext(), magnitudeColorResourceId);
     }
 }
